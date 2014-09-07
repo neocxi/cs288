eebbb4e874a83fd80c83a562c611b64bc14de378
Rocky Duan <dementrock@gmail.com>
2014-09-06 21:12:56 -0700

    some playing around

--- proj1/test.sh
+++ proj1/test.sh
@@ -1,1 +1,1 @@
-java -cp assign1.jar:target/scala-2.11/assign1-submit.jar -server -mx2000m edu.berkeley.nlp.assignments.assign1.LanguageModelTester -path ../assign1_data -lmType UNIGRAM -sanityCheck
+java -cp assign1.jar:target/scala-2.11/assign1-submit.jar -server -mx2000m edu.berkeley.nlp.assignments.assign1.LanguageModelTester -path data -lmType ${1:-TRIGRAM} # -sanityCheck
